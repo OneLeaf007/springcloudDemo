8cbbfebe54dff9075a9e32f9baf4a6c9bc8abc56
liuhuabing <240303928@qq.com>
2020-09-15 17:02:34 +0800

    rep

--- mySpringCloudService-api/src/main/java/com/mystudy/springCloud/common/Dept.java
+++ mySpringCloudService-api/src/main/java/com/mystudy/springCloud/common/Dept.java
@@ -4,4 +4,31 @@ package com.mystudy.springCloud.common;
 
 public class Dept {
 
+    int age ;
+
+    String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
